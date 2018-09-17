71230a45afa80c311a34f0f65063c54720e6edf0
Professor Philip Lewis <dr.p.e.lewis@gmail.com>
2018-09-17 14:23:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-"""A setuptools based setup module.
+"""setuptools for geog0111 Scientific Computing, UCL 
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+https://github.com/profLewis/geog0111
 """
 
 # Always prefer setuptools over distutils
@@ -29,7 +27,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/profLewis/geog0111',
 
     # Author details
     author='Prof. P. Lewis',
@@ -55,17 +53,11 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='sceintific computing',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -75,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
+    install_requires=['numpy', 'matplotlib', 'pandas', 'gdal'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
