804678aa3a3589b714ecc0c0b7421194325a9dae
Professor Philip Lewis <dr.p.e.lewis@gmail.com>
2018-09-17 14:36:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,11 +53,12 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
-    keywords='sceintific computing',
+    keywords='scintific computing',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
