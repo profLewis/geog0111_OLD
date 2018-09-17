7784dde508286c8df3c7378d890ae383466d3c40
Professor Philip Lewis <dr.p.e.lewis@gmail.com>
2018-09-17 15:11:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'matplotlib', 'pandas', 'jupyter'],
+    install_requires=['numpy', 'matplotlib', 'pandas', 'jupyter','gdal'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
