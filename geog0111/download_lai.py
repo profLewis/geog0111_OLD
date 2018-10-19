70a7e60e8f26d5e809f5a53d07ad198486fef117
Philip Lewis <dr.p.e.lewis@gmail.com>
2018-10-19 21:14:02 +0100

    updated from geog0111

--- geog0111/download_lai.py
+++ geog0111/download_lai.py
@@ -8,13 +8,13 @@ for given tiles into destination_folder
 from geog0111.get_modis_files import get_modis_files
 from datetime import datetime
 
-years = [2018,2017]
+years = [2017,2016]
 tiles = ['h17v03', 'h18v03','h17v04', 'h18v04']
 destination_folder = 'data'
 
 for year in years:
     ndoys = (datetime(year,12,31) - datetime(year,1,1)).days + 1
-    for doy in range(1,ndoys+1):
+    for doy in range(109+4,ndoys+1,4):
         print(year,ndoys,doy,end=' ')
         filenames = get_modis_files(doy,year,tiles,base_url='https://e4ftl01.cr.usgs.gov/MOTA',\
                                            version=6,\
