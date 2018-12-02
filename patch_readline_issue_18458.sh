52c37f9ec3fc699ad7125a34bce8b68ed93ada31
Philip Lewis <dr.p.e.lewis@gmail.com>
2018-12-02 19:55:41 +0000

    updated from geog0111

---
+++ patch_readline_issue_18458.sh
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>301 Moved Permanently</title>
+</head><body>
+<h1>Moved Permanently</h1>
+<p>The document has moved <a href="https://bugs.python.org/file32324/patch_readline_issue_18458.sh">here</a>.</p>
+<hr>
+<address>Apache/2.2.16 (Debian) Server at bugs.python.org Port 80</address>
+</body></html>
